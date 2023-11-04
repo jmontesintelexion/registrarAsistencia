1c3780d6bc9d2ce7ab7d8ac323c9cec99f4235db
José Luis Montes de Oca Muñoz <jmontes@Intelexion.com>
2023-11-04 03:17:27 -0600

    subiendo yml

--- cypress/integration/checarAsistencia.spec.js
+++ cypress/integration/checarAsistencia.spec.js
@@ -23,6 +23,7 @@ describe('Checar asistencia', () => {
 
     cy.get('.sidebar-toggle-button').click()
     cy.get('#container > div > div > div > div > div > div:nth-child(1) > div:nth-child(5) > ul > li:nth-child(7) > span').click()
+    cy.wait(10000)
     cy.get('#RegistrarAsistencia > div > div > div.card-body > div > button:nth-child(1)').contains('Oficina 1')
   })
 
