dab91426f02535116d52899f05f5980109f9ae90
José Luis Montes de Oca Muñoz <jmontes@Intelexion.com>
2023-11-07 02:58:56 -0600

    valido columna de fechas para fecha de hoy

--- cypress/integration/checarAsistencia.spec.js
+++ cypress/integration/checarAsistencia.spec.js
@@ -27,6 +27,44 @@ describe('Checar asistencia', () => {
     cy.get('#RegistrarAsistencia > div > div > div.card-body > div > button:nth-child(1)').contains('Oficina 1')
     cy.wait(15000)
 
+    var d, m, y, i, t;
+    var td = new Date();
+
+    d = td.getDate().toString().padStart(2, '0')
+    m = (td.getMonth() + 1).toString().padStart(2, '0')
+    y = td.getFullYear().toString()
+
+    var today = (d + "/" + m + "/" + y).toString()
+
+    cy.log("La fecha de hoy es: " + today)
+
+
+    cy.get('td.column_0').each(($el, index) => {
+
+      if($el.text() == today){
+
+        i = (index + 1).toString() //INCREMENTA EL NUMERO PARA TOMAR EL ELEMENTO CORRECTO
+        cy.log("El index que toma es: " + i)
+
+        var txtel = ":nth-child(" + i + ") > .column_1 > div"
+
+        cy.get(txtel).then(function(e){
+          t = Cypress.$(e).text()
+          cy.log("El estatus de la fecha del el es: " + t)
+
+          if(t.toString() == "Falta"){
+            cy.log("HAGO DE CLICK PARA LA ASISTENCIA")
+            cy.get('#RegistrarAsistencia > div > div > div.card-body > div > button:nth-child(1)').click()
+          }
+          else{
+            cy.log("YA SE REGISTRÓ LA ASISTENCIA")
+          }
+  
+        })
+      }
+    })
+
+    /*
       var date, today;
       var t = ""
       var d, m, y;
@@ -68,6 +106,7 @@ describe('Checar asistencia', () => {
             cy.log("NO ES EL DÍA DE HOY")
           }
       })
+      */
   })
 
 
